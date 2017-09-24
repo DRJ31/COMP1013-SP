075e483b9e6eff5fa1eeac7abb3c81a06d44f1d8
dengrenjie <dengrenjie31@gmail.com>
2017-09-24 20:11:08 +0800

    first commit

---
+++ Lab1/HelloWorld1.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void){
+	printf("Hello World\n");//1st
+	printf("Hello World\n");//2nd
+	printf("Hello World\n");//3rd
+	return 0;
+}
---
+++ Lab1/HelloWorld2.cpp
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void){
+	printf("Hello World\nHello World\nHello World\n");
+	return 0;
+}
