fc0e6714c159b0e496f7c1821a1128e397523e5e
dengrenjie <dengrenjie31@gmail.com>
2017-09-24 20:58:22 +0800

    Lab2

---
+++ Lab2/task1.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void){
+	printf("Hello\n");
+	// return 0;
+	printf("World\n");
+	return 0;
+	// return 1;
+}
---
+++ Lab2/task2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+    printf("Number of bytes used to store a char type variable is %d\n", sizeof(char));
+    printf("Number of bytes used to store a short type variable is %d\n", sizeof(short));
+    printf("Number of bytes used to store a int type variable is %d\n", sizeof(int));
+    printf("Number of bytes used to store a long type variable is %d\n", sizeof(long));
+    printf("Number of bytes used to store a float type variable is %d\n", sizeof(float));
+    printf("Number of bytes used to store a double type variable is %d\n", sizeof(double));
+    return 0;
+} 
