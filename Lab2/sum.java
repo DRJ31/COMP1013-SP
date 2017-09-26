7e49e6f9ac6dda2eb61d6080c1d0a83eb1b1872e
dengrenjie <dengrenjie31@gmail.com>
2017-09-26 12:39:07 +0800

    java

---
+++ Lab2/sum.java
@@ -0,0 +1,12 @@
+public class sum{
+	public static void main(String[] args){
+		int value1, value2, sum;
+		//float value1, value2, sum;
+		value1 = 50;
+		value2 = 25;
+		sum = value1 + value2;
+		System.out.printf("The sum of %d and %d is %d\n", value1, value2, sum);
+		//System.out.printf("The sum of %f and %f is %f\n", value1, value2, sum);
+		//System.out.printf("The sum of %.1f and %.1f is %.1f\n", value1, value2, sum);
+	}
+}
---
+++ Lab2/task2.java
@@ -0,0 +1,10 @@
+public class task2{
+	public static void main(String[] args){
+		System.out.println("Number of bytes used to store a char type variable is " + Character.SIZE/8);
+		System.out.println("Number of bytes used to store a short type variable is " + Short.SIZE/8);
+		System.out.println("Number of bytes used to store a int type variable is " + Integer.SIZE/8);
+		System.out.println("Number of bytes used to store a long type variable is " + Long.SIZE/8);
+		System.out.println("Number of bytes used to store a float type variable is " + Float.SIZE/8);
+		System.out.println("Number of bytes used to store a double type variable is " + Double.SIZE/8);
+	}
+}
