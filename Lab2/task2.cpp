51b5c7635ab9ded8e69f6e655eea3cdff76d655e
dengrenjie <dengrenjie31@gmail.com>
2017-09-29 08:05:27 +0800

    edited task2.cpp

--- Lab2/task2.cpp
+++ Lab2/task2.cpp
@@ -6,5 +6,6 @@ int main(void){
     printf("Number of bytes used to store a long type variable is %d\n", sizeof(long));
     printf("Number of bytes used to store a float type variable is %d\n", sizeof(float));
     printf("Number of bytes used to store a double type variable is %d\n", sizeof(double));
+    printf("Number of bytes used to store a bool type variable is %d\n", sizeof(bool));
     return 0;
 } 
