53eb06f947091258245d5c87390cee1b1ed7bcc0
dengrenjie <dengrenjie31@gmail.com>
2017-10-17 11:00:48 +0800

    update option

--- Lab4/opt.cpp
+++ Lab4/opt.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
-int bitnum(int i, int pos);
+int bitnum(int num, int pos);
 int main(){
-    int a, result=0, i;
+    int a;
     scanf("%d", &a);
     printf("%d is %d%d%d%d in binary.\n", a, bitnum(a, 1), bitnum(a, 2), bitnum(a, 3), bitnum(a, 4));
     return 0;
 }
-int bitnum(int i, int pos){
-    return (i >> (4 - pos)) % 2;
+int bitnum(int num, int pos){
+    return (num >> (4 - pos)) % 2;
 }
