16534b329947ae641a29ffb340ec13a239ca070e
DRJ31 <dengrenjie31@gmail.com>
2017-10-31 12:53:00 +0800

    fix option2.cpp

--- Optional1(Judy)/option2.cpp
+++ Optional1(Judy)/option2.cpp
@@ -1,13 +1,22 @@
 #include <stdio.h>
-int main(){
+int main() {
 	int i, j;
-	for (i = 1; i <= 7; i++){
-		for (j = 0; j < i; j++){
+	for (i = 1; i <= 7; i++) {
+		for (j = 0; j < i; j++) {
 			if (i % 2)
 				printf("# ");
 			else
 				printf("+ ");
 		}
+		puts("");
 	}
-	for (i = 6; i)
+	for (i = 6; i > 0; i--) {
+		for (j = 0; j < i; j++) {
+			if (i % 2)
+				printf("# ");
+			else
+				printf("+ ");
+		}
+		puts("");
+	}
 }
