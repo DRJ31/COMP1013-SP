a5fb3a1aed2500a257a43b345dcb271532069565
dengrenjie <dengrenjie31@gmail.com>
2017-11-04 22:11:23 +0800

    optional2 java done

--- Optional2/t2.cpp
+++ Optional2/t2.cpp
@@ -5,17 +5,17 @@ int main(){
     for (int i = 3; i < 1000; i += 3){
         if (checkNumber(i))
             printf("%d [OK]\n", i);
+        else
+            printf("%d [failed]\n", i);
     }
     return 0;
 }
 int checkNumber(int number){
-    int i, j = 0, result = 0;
+    int j = 0, result = 0;
     while(j < 50){
-        i = 0;
         while(number >= 1){
             result += cube(number % 10);
             number /= 10;
-            i++;
         }
         if (result != 153){
             number = result;
