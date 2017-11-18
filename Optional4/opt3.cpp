a6e6cc2f185637da7a0c3efe51c2ba85092e23f2
dengrenjie <dengrenjie31@gmail.com>
2017-11-19 00:39:21 +0800

    add float in cpp

--- Optional4/opt3.cpp
+++ Optional4/opt3.cpp
@@ -1,34 +1,52 @@
 #include <stdio.h>
 #include <string.h>
-int exp(int num, int expo);
-int resultNum(char num[], int length);
-int main(){
-    char a[100];
-    scanf("%s", a);
-    printf("The number is %d\n", resultNum(a, strlen(a)));
-    return 0;
+double exp(double num, int expo);
+double resultIntNum(char num[], int length);
+int checkPointPos(char str[]);
+double resultFloatNum(char num[], int begin, int length);
+int main() {
+	char a[100];
+	scanf("%s", a);
+	if (!checkPointPos(a))
+		printf("The number is %g\n", resultIntNum(a, strlen(a)));
+	else 
+		printf("The number is %g\n", resultIntNum(a, checkPointPos(a)) + resultFloatNum(a, checkPointPos(a), strlen(a)));
+	return 0;
 }
-int resultNum(char num[], int length){
-    int result = 0;
-    if (num[0] <= '9' && num[0] >= '0'){
-        for (int i = 0; i < length; i++)
-            result += ((int)num[length-i-1]-48) * exp(10, i);
-    }
-    else{
-        for (int i = 0; i < length - 1; i++)
-            result += ((int)num[length-i-1]-48) * exp(10, i);
-        if (num[0] == '+')
-            return result;
-        else if (num[0] == '-')
-            return -result;
-        else
-            return 0;
-    }
-    return result;
+double resultIntNum(char num[], int length) {
+	double result = 0;
+	if (num[0] <= '9' && num[0] >= '0') {
+		for (int i = 0; i < length; i++)
+			result += ((int)num[length - i - 1] - 48) * exp(10, i);
+	}
+	else {
+		for (int i = 0; i < length - 1; i++)
+			result += ((int)num[length - i - 1] - 48) * exp(10, i);
+		if (num[0] == '+')
+			return result;
+		else if (num[0] == '-')
+			return -result;
+		else
+			return 0;
+	}
+	return result;
 }
-int exp(int num, int expo){
-    int result = 1;
-    for (int i = 0; i < expo; i++)
-        result *= num;
-    return result;
+double exp(double num, int expo) {
+	double result = 1;
+	for (int i = 0; i < expo; i++)
+		result *= num;
+	return result;
+}
+int checkPointPos(char str[]) {
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i] == '.')
+			return i;
+	}
+	return 0;
+}
+double resultFloatNum(char num[], int begin, int length) {
+	double result = 0;
+	for (int i = begin + 1; i < length; i++) 
+		result += ((int)num[i] - 48) * exp(0.1, i - begin);
+	return result;
 }
