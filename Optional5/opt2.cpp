d8dabc75ee5eac9eb3663b9c27abdd876d590e2b
dengrenjie <dengrenjie31@gmail.com>
2017-11-24 15:24:19 +0800

    Judy lab8 done

--- Optional5/opt2.cpp
+++ Optional5/opt2.cpp
@@ -9,8 +9,8 @@ int main(){//Input 0 to stop scanning.
         if (numbers[i] == 0)
             break;
     }
-    bubbleSort(numbers, i-1);
-    for (int j = 0; j < i-1; j++)
+    bubbleSort(numbers, i);
+    for (int j = 0; j < i; j++)
         printf("%d ", numbers[j]);
     puts("");
     return 0;
@@ -20,7 +20,7 @@ void bubbleSort(int numbers[], int length){
     do{
         status = 0;
         for (int i = 1; i < length; i++){
-            if (numbers[i-1] > numbers[i]){
+            if (numbers[i-1] < numbers[i]){
                 int tmp = numbers[i-1];
                 numbers[i-1] = numbers[i];
                 numbers[i] = tmp;
--- Optional5/opt3.cpp
+++ Optional5/opt3.cpp
@@ -1,4 +1,34 @@
 #include <stdio.h>
 #include <string.h>
-void bubbleSort()
-int main()
+void bubbleSort(char str[20][20], int length);
+int main(){
+    char str[20][20];
+    char check;
+    int i;
+    puts("Please input words: ");
+    for (i = 0; i < 20; i++){
+        scanf("%s", str[i]);
+        check = getchar();
+        if (check == '\n')
+            break;
+    }
+    bubbleSort(str, i+1);
+    for (int j = 0; j <= i; j++)
+        printf("%s ", str[j]);
+    puts("");
+    return 0;
+}
+void bubbleSort(char str[20][20], int length){
+    int status;
+    do{
+        status = 0;
+        for (int i = 1; i < length; i++){
+            if (strcmp(str[i-1], str[i]) > 0){
+                char tmp[20];
+                strcpy(tmp, str[i-1]);
+                strcpy(str[i-1], str[i]);
+                strcpy(str[i], tmp);
+            }
+        }
+    }while (status != 0);
+}
