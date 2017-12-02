18def5f693afa0512fa84747efd0a3e4dcb8a610
dengrenjie <dengrenjie31@gmail.com>
2017-12-02 23:16:45 +0800

    Lab10 cpp version done

--- Lab10/t3.cpp
+++ Lab10/t3.cpp
@@ -4,14 +4,14 @@ int main(){
     int n;
     puts("Please input a positive integer: ");
     scanf("%d", &n);
-    if (n <= 0){
+    if (n <= 0){//Remind user to input a positive integer
         puts("Please input a positive integer.");
         return 0;
     }
     printf("T(%d)=%d\n", n, func(n));
     return 0;
 }
-int func(int n){
+int func(int n){//Calculate T(n)
     if (n == 1)
         return 1;
     else if (n > 1)
--- Lab10/t4.cpp
+++ Lab10/t4.cpp
@@ -1,13 +1,28 @@
 #include <stdio.h>
-int binarySearch(int A[], int x, int l, int r);
+int binarySearch(int A[], int x, int l, int r);//Use binary search to search for numbers
 int main(){
+    int arr[] = {-34, -33, -21, -13, -2, 3, 4, 6, 9, 12, 21, 23, 25, 32, 42, 48, 50}, x;
+    puts("The array is: ");
+    int arrLength = sizeof(arr) / sizeof(int);//Length of the array
+    for (int i = 0; i < arrLength - 1; i++)//Print the array
+        printf("%d, ", arr[i]);
+    printf("%d\n", arr[arrLength-1]);
+    puts("Please input an integer: ");
+    scanf("%d", &x);
+    if (binarySearch(arr, x, 0, arrLength) != -1)
+        printf("%d's position in the array is: %d\n", x, binarySearch(arr, x, 0, arrLength));
+    else
+        printf("%d is not in this array.\n", x);
     return 0;
 }
 int binarySearch(int A[], int x, int l, int r){
-    if (x == A[r])
-        return r;
-    else if (A[r] < x)
-        return binarySearch(A, x, l, (r-l) / 2 + l);
-    else
-        return binarySearch(A, x, , r);
+    int pos = (l + r) / 2;
+    if (l > r)//When the number cannot be found, break recursive
+        return -1;
+    if (x == A[pos])
+        return pos;
+    else if (A[pos] > x)
+        return binarySearch(A, x, l, pos - 1);
+    else//A[pos] < x
+        return binarySearch(A, x, pos + 1, r);
 }
