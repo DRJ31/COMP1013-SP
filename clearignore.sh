d2de7996ff74555ec92e04260ac8dde19ead698d
dengrenjie <dengrenjie31@gmail.com>
2017-12-07 22:00:43 +0800

    finally done clearignore.sh

--- clearignore.sh
+++ clearignore.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-find . -name $1
-echo $1
+tmp=`cat .gitignore | grep "*"`
+read -a arr <<< ${tmp[@]}
+length=${#arr[@]}
+let length--
+for i in `seq 1 $length`
+do
+	long=${#arr[$i-1]}
+	let long--
+	find . -name ${arr[$i-1]:0:$long} | tee -a log.txt | xargs rm -rf
+done
+find . -name ${arr[$length]} | tee -a log.txt | xargs rm -rf
--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in `cat .gitignore | grep "*"`
-do
-    ./clearignore.sh $i &
-    echo $! | xargs wait
-done
