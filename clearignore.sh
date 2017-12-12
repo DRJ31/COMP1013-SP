8e1146185d1dc0ec0acde22b6800cf1994548c1f
dengrenjie <dengrenjie31@gmail.com>
2017-12-12 15:12:30 +0800

    modify clearignore

--- clearignore.sh
+++ clearignore.sh
@@ -2,11 +2,11 @@
 tmp=`cat .gitignore | grep "*"`
 read -a arr <<< ${tmp[@]}
 length=${#arr[@]}
-let length--
+#let length--
 for i in `seq 1 $length`
 do
 	long=${#arr[$i-1]}
 	let long--
-	find . -name ${arr[$i-1]:0:$long} | tee -a log.txt | xargs rm -rf
+	find . -name "${arr[$i-1]:0:$long}" | tee -a log.txt | xargs rm -rf
 done
-find . -name ${arr[$length]} | tee -a log.txt | xargs rm -rf
+#find . -name "${arr[$length]}" | tee -a log.txt | xargs rm -rf
