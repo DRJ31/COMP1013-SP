93788fbcaed1bcd8fa58a80d7978546179ea84d8
Renjie Deng <dengrenjie31@gmail.com>
2017-12-12 15:44:14 +0800

    update clearignore

--- clearignore.sh
+++ clearignore.sh
@@ -7,6 +7,8 @@ for i in `seq 1 $length`
 do
 	long=${#arr[$i-1]}
 	let long--
-	find . -name "${arr[$i-1]:0:$long}" | tee -a log.txt | xargs rm -rf
+	find . -name "${arr[$i-1]:0:$long}" | tee log.txt | xargs rm -rf
 done
+cat log.txt
+rm -f log.txt
 #find . -name "${arr[$length]}" | tee -a log.txt | xargs rm -rf
