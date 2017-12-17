b42e21feccfbe56df3fe1325a737157576022f33
dengrenjie <dengrenjie31@gmail.com>
2017-12-17 09:52:46 +0800

    fix problem in ass5

--- Ass5/t1.cpp
+++ Ass5/t1.cpp
@@ -1,20 +1,21 @@
 #include <stdio.h>
 /*
 This program receive numbers of double type and output them.
-Input 0 to stop scanning.
+Input the numbers in a line and press enter to stop input.
 */
 int main(){
     double numbers[20];
     int i;
-    puts("Please input no more than 20 numbers (Enter 0 to stop): ");
+    puts("Please input no more than 20 numbers in one line (Press Enter to stop): ");
     for (i = 0; i < 20; i++){
         scanf("%lf", &numbers[i]);
-        if (numbers[i] == 0)
+        if (getchar() == '\n')
             break;
     }
     double *pt = numbers;//Initialize the pointer of the array
     for (int j = 0; j <= i; j++){
-        printf("%g ", *(pt+j));
+        printf("The value is: %g, the address is: %p\n", *pt, pt);
+        pt++;
     }
     puts("");
     return 0;
